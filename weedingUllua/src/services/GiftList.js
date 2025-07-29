b57efcd4a4b94b7d3d54d83e46c7c598a82a7490
Santiago Alvarez <santiagoalvarez290401@gmail.com>
2025-07-29 18:09:39 -0300

    Update GiftList.js

--- weedingUllua/src/services/GiftList.js
+++ weedingUllua/src/services/GiftList.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getGiftList = async() =>{
     
     try {
-        const response = await axios(`https://api.sheetbest.com/sheets/56ab91ba-b2b8-4958-8804-a1aea152c123`);
+        const response = await axios(`https://api.sheetbest.com/sheets/904e525e-c72b-4e47-89e3-6487a0346d88`);
         response.data = response.data.filter(e=> e.invitado === 'no')
         return response
     
@@ -19,11 +19,11 @@ export const updateItem = async(id, name) =>{
     
     try {
         //valido que no haylla sido elegido aun, si lo fue envio un mensaje al modal
-        const response = await axios(`https://api.sheetbest.com/sheets/56ab91ba-b2b8-4958-8804-a1aea152c123/id/${id}`);
+        const response = await axios(`https://api.sheetbest.com/sheets/904e525e-c72b-4e47-89e3-6487a0346d88/id/${id}`);
         
         if (response.data[0].invitado === 'no') {
             //actualizo y envio mensaje de exito
-            const updateGift = await axios.patch(`https://api.sheetbest.com/sheets/56ab91ba-b2b8-4958-8804-a1aea152c123/id/${id}`,{'invitado':name})
+            const updateGift = await axios.patch(`https://api.sheetbest.com/sheets/904e525e-c72b-4e47-89e3-6487a0346d88/id/${id}`,{'invitado':name})
 
             if (updateGift.status===200){
                 return true 
